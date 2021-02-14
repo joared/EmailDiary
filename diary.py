c4dac72d18a6b6d4102a223e36494ad22ad29501
Joar Edlund <jedlund93@gmail.com>
2021-02-14 21:12:17 +0100

    First commit

---
+++ diary.py
@@ -0,0 +1,49 @@
+import datetime
+
+class Entry:
+    def __init__(self, raw_entry):
+        self.date, self.text = self._parse(raw_entry)
+        
+    def _parse(self, raw_entry):
+        raw_entry = raw_entry.strip()
+        #print("Date string: " + raw_entry[:raw_entry.find("\n")])
+        date_string = raw_entry.split()[1]
+        date = datetime.datetime(*map(int, date_string.split("/")))
+        text = raw_entry[raw_entry.index(date_string) + len(date_string):].strip()
+        return date, text
+
+class Diary:
+    def __init__(self):
+        self.entries = []
+        
+    def add_entry(self, entry):
+        if isinstance(entry, Entry):
+            self.entries.append(entry)
+        elif isinstance(entry, str):
+            self.entries.append(Entry(entry))
+            
+    def get_tags(self, tag_symbol):
+        names = []
+        for e in self.entries:
+            for n in get_tags_from_entry(e.text, tag_symbol):
+                if n not in names:
+                    names.append(n)
+        return names
+
+def get_tags_from_entry(entry, tag_symbol):
+    names = []
+    entry_temp = entry
+    for _ in range(entry.count(tag_symbol)):
+        idx = entry_temp.index(tag_symbol)
+        entry_temp = entry_temp[idx+1:]
+        
+        name = entry_temp.split()[0].split(",")[0].split(".")[0]
+        if len(entry_temp.split()[0]) < 2:
+            name += entry_temp.split()[1].split(",")[0].split(".")[0]
+        
+        if not name in names: 
+            names.append(name)
+    return names
+
+if __name__ == "__main__":
+    pass
---
+++ main.py
@@ -0,0 +1,73 @@
+from quickstart import search_message, get_message, get_service, cleantext
+from diary import Diary
+
+def get_diary(service, user_id, msg_ids):
+    d = Diary()
+    for id in enumerate(msg_ids):
+        # wtf happened to the ids? (somenumber, id)
+        message = get_message(service, user_id, id[1])
+        for m in cleantext(message):
+            d.add_entry(m)
+    
+    return d
+
+def choose_msg(service, user_id, msg_ids):
+    messages = []
+    
+    for id in enumerate(msg_ids):
+        # wtf happened to the ids? (somenumber, id)
+        message = get_message(service, user_id, id[1])
+        messages_ls = cleantext(message)
+        messages.extend(messages_ls)
+       
+    for i, m in enumerate(messages):
+        print("{}. {}".format(i+1, m[:50].strip().replace("\n", " ")))
+        
+    choice = int(input(": "))
+    return messages[choice-1]
+
+def show_entries(diary):
+    for entry in d.entries:
+        print("{} - {}".format(entry.date.date(), entry.text[:50].replace("\n", " ")))
+        
+def show_tags(diary):
+    _show_tags(diary, "#")
+    
+def show_people(diary):
+    _show_tags(diary, "@")
+        
+def _show_tags(diary, tag_symbol):
+    for i, n in enumerate(diary.get_tags(tag_symbol)):
+        print("{}. {}".format(i+1, n))
+
+if __name__ == "__main__":
+    """
+    search operators: https://support.google.com/mail/answer/7190?hl=sv
+    tutorial: https://www.youtube.com/watch?v=njDGaVnz9Z8
+    """
+    service = get_service()
+    user_id = "me"
+
+    #search_string = input("Search string: ")
+    search_string = "subject:Mydiary"
+    message_ids = search_message(service, user_id, search_string)
+    #messages = choose_msg(service, user_id, message_ids)
+    d = get_diary(service, user_id, message_ids)
+    
+    choices = [show_entries, show_people, show_tags]
+    l = len(choices)
+    while True:
+        print("What you wanna do?")
+        for i in range(l):
+            print("{}. {}".format(i+1, choices[i].__name__))
+        print("{}. Exit".format(l+1))
+        choice = int(input(": "))
+        if choice == l+1:
+            break
+        elif choice < l+1 and choice >0:
+            choices[choice-1](d)
+        else:
+            print("Invalid choice")
+        
+
+    
